236f4b201bf8eeb6e4de118a945fe839106636ca
n-mariana <mariana.sgoncalves2010@gmail.com>
2026-01-19 20:59:49 -0300

    Started making UI

---
+++ src/br/com/dio/UIMain.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import br.com.dio.ui.custom.screen.MainScreen;
+
+
+public class UIMain {
+
+    public static void main(String[] args) {
+        final var gameConfig = Stream.of(args)
+            .collect(Collectors.toMap(k -> k.split(";")[0],v -> v.split(";")[1]));
+        var mainScreen = new MainScreen(gameConfig);
+        mainScreen.buildMainScreen();
+    }
+
+}
